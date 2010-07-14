bbf22d7c1d51ad0e7dcc2df7a3759fa2b08f31f9
David Vollbracht <david.vollbracht@gmail.com>
2010-07-14 15:05:38 -0400

    Setup basic blaze example for echo.  Remove heist template rendering

--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,13 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import           Prelude
+import qualified Prelude as P
 import           Control.Applicative
+import           Data.ByteString
+
 import           Snap.Types
+import qualified Snap.Types as T
 import           Snap.Util.FileServe
 import           Text.Templating.Heist
 import           Text.Templating.Heist.TemplateDirectory
@@ -10,21 +15,28 @@ import           Text.Templating.Heist.TemplateDirectory
 import           Glue
 import           Server
 
+import           Text.Blaze.Html5
+import           Text.Blaze.Html5.Attributes
+import qualified Text.Blaze.Html5 as H
+import qualified Text.Blaze.Html5.Attributes as A
 
 main :: IO ()
-main = do
-    td <- newTemplateDirectory' "templates" emptyTemplateState
-    quickServer $ templateHandler td defaultReloadHandler $ \ts ->
+main = quickServer $ 
         ifTop (writeBS "hello world") <|>
-        route [ ("foo", writeBS "bar")
-              , ("echo/:echoparam", echoHandler)
+        route [ ("echo/:echoparam", echoHandler)
               ] <|>
-        templateServe ts <|>
-        dir "static" (fileServe ".")
+        T.dir "static" (fileServe ".")
 
 
 echoHandler :: Snap ()
 echoHandler = do
     param <- getParam "echoparam"
-    maybe (writeBS "must specify echo/param in URL")
-          writeBS param
+    writeLBS $ renderHtml (echoView param)
+
+echoView :: Maybe ByteString -> Html a
+echoView s = html $ do
+     H.head $ do
+         H.title "Echo"
+     body $
+        p . showHtml $
+          maybe ("must specify echo/param in URL") P.id s
