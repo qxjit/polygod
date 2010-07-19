290e494f5bdc8f1b8c9022a0dbb5f618178c7f4b
David Vollbracht <david.vollbracht@gmail.com>
2010-07-19 00:28:52 -0400

    Refactor updateWorldHandler to make error handling look a bit cleaner

--- src/WorldHandlers.hs
+++ src/WorldHandlers.hs
@@ -16,12 +16,13 @@ import           Timeline
 updateWorldHandler :: Timeline a -> Snap ()
 updateWorldHandler timeline = do
   body <- getRequestBody
-  either (\_ -> badPost "The post data was not valid JSON.  Please use JSON format to post updates to the world.\n")
-         (\json -> maybe (badPost "The JSON you posted didn't include any properly structured cell data.\n")
-                         (\newCells -> do liftIO $ interfere (updateCells newCells) timeline
-                                          writeBS "Divine intervention successful.  Happy godding!\n")
-                         (jsonToCells json))
-         (Json.decode (Strict.concat . Lazy.toChunks $ body))
+  processInput (Json.decode (Strict.concat . Lazy.toChunks $ body))
+               "The post data was not valid JSON.  Please use JSON format to post updates to the world.\n" $
+
+    \json -> processInput (jsonToCells json) "The JSON you posted didn't include any properly structured cell data.\n" $
+
+      \newCells -> do liftIO $ interfere (updateCells newCells) timeline
+                      writeBS "Divine intervention successful.  Happy godding!\n"
 
 worldHandler :: Timeline (Snap ()) -> Snap ()
 worldHandler timeline =  do
@@ -38,11 +39,24 @@ nextWorldHandler timeline = do
                     _ -> pass)
         tickParam
 
-badPost :: Strict.ByteString -> Snap ()
-badPost message = do
+badRequest :: Strict.ByteString -> Snap ()
+badRequest message = do
         putResponse $
           setResponseStatus 400 "Bad Request" emptyResponse
         writeBS message
         r <- getResponse
         finishWith r
 
+processInput :: PossibleBadInput m => m b -> Strict.ByteString -> (b -> Snap()) -> Snap ()
+processInput input message action = useGoodInput input action (badRequest message)
+
+class PossibleBadInput m where
+  useGoodInput :: m a -> (a -> b) -> b -> b
+
+instance PossibleBadInput Maybe where
+  useGoodInput (Just a) f _ = f a
+  useGoodInput _ _ b = b
+
+instance PossibleBadInput (Either a) where
+  useGoodInput (Right a) f _ = f a
+  useGoodInput _ _ b = b
